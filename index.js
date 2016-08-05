31b3502ef82615cb6524ad9f79d8bc144b1e4499
Nigel Wade <nigeljohnwade@gmail.com>
2016-08-05 11:14:33 +0100

    Twitter streaming API parameters are OR not AND combined

--- index.js
+++ index.js
@@ -4,28 +4,27 @@ var config = require('./config.js');
 //User based authentication
 
 var client = new Twitter({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token_key,
-  access_token_secret: config.access_token_secret
+    consumer_key: config.consumer_key,
+    consumer_secret: config.consumer_secret,
+    access_token_key: config.access_token_key,
+    access_token_secret: config.access_token_secret
 });
-
-//REST API
-//client.get('favorites/list', function(error, tweets, response) {
-  //if(error) throw error;
-  //console.log(tweets);  // The favorites. 
-  //console.log(response);  // Raw response object. 
-//});
 
 //Streaming API
-var stream = client.stream('statuses/filter', {follow:27426152,track:'javacsript'});
+var stream = client.stream('statuses/filter', {follow:27426152});
 stream.on('data', function(event) {
-  console.log(event);
-  client.post('statuses/update', {status: '@nigeljohnwade Sorry, *javascript*', in_reply_to_status_id: event.id}, function(err, tweet){
-     if(!err) console.log(tweet) 
-  });
+    console.log(event);
+    if(event.text.indexOf('javacsript') > -1) {
+        console.log(event);
+        client.post('statuses/update',
+            {status: '@nigeljohnwade Sorry, *javascript*', in_reply_to_status_id: event.id},
+            function (err, tweet) {
+                if (!err) console.log(tweet)
+            }
+        );
+    }
 });
- 
+
 stream.on('error', function(error) {
-  throw error;
+    throw error;
 });
